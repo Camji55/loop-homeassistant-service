51dffd07cf7fd6f3d2448651c644e013b4607c78
Cameron Ingham <camji555@gmail.com>
2026-07-22 10:45:35 -0700

    initial commit

--- HomeAssistantServiceKitUI/HomeAssistantService+UI.swift
+++ HomeAssistantServiceKitUI/HomeAssistantService+UI.swift
@@ -10,17 +10,17 @@ import LoopKit
 import LoopKitUI
 import HomeAssistantServiceKit
 
-extension HomeAssistantService: ServiceUI {
+extension HomeAssistantService: @retroactive ServiceUI {
 
     public static var image: UIImage? {
         return UIImage(named: "home-assistant", in: Bundle(for: HomeAssistantServiceNavigationController.self), compatibleWith: nil)
     }
 
-    public static func setupViewController(colorPalette: LoopUIColorPalette, pluginHost: PluginHost) -> SetupUIResult<ServiceViewController, ServiceUI> {
+    public static func setupViewController(colorPalette: LoopUIColorPalette, pluginHost: any PluginHost, allowDebugFeatures: Bool) -> SetupUIResult<any ServiceViewController, any ServiceUI> {
         return .userInteractionRequired(HomeAssistantServiceNavigationController(service: HomeAssistantService(), forCreation: true, colorPalette: colorPalette))
     }
 
-    public func settingsViewController(colorPalette: LoopUIColorPalette) -> ServiceViewController {
+    public func settingsViewController(colorPalette: LoopUIColorPalette, allowDebugFeatures: Bool) -> any ServiceViewController {
         return HomeAssistantServiceNavigationController(service: self, forCreation: false, colorPalette: colorPalette)
     }
 }
